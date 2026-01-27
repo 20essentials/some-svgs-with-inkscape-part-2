8c832616e5c1a34f65907cf246f4eebc664297e9
DominicDev <s0l0estudi4r@gmail.com>
2026-01-27 14:33:22 +0100

    v0.0.5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   build: {
@@ -8,9 +10,10 @@ export default defineConfig({
   },
 
   //Localhost
-  site: 'http://localhost:4321/'
-
   //Deploy normal project
   // base: '/project-1020/',
   // site: 'https://20essentials.github.io/project-1020/'
+  site: 'http://localhost:4321/',
+
+  integrations: [react()]
 });
---
+++ src/components/ContainerThreads.tsx
@@ -0,0 +1,9 @@
+import Threads from './Threads';
+
+export const ContainerThreads = () => {
+  return (
+    <article className='ContainerThreads'>
+      <Threads amplitude={1.2} distance={0} enableMouseInteraction={true} />
+    </article>
+  );
+};
