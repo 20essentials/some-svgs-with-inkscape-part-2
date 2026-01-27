e861209091a33419e9f3c73fc713ca00d97d0e35
DominicDev <s0l0estudi4r@gmail.com>
2026-01-27 14:21:42 +0100

    v0.0.3

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -2,3 +2,4 @@ export const TOTAL_SVGS = 69;
 export const arraySvgs = Array.from({ length: TOTAL_SVGS }, (_, i) => ({
   url: `/assets/${i + 1}/${i + 1}.svg`
 }));
+export const TITLE = `${TOTAL_SVGS} SVGs with Inkscape - Part 2`
