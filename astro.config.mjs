0bf7896082ef64bd1e8597cb9a62767c0aef4dd5
DominicDev <s0l0estudi4r@gmail.com>
2026-04-05 15:02:28 +0200

    v0.2.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 // https://astro.build/config
 export default defineConfig({
   build: {
     assets: 'static'
   },
-
-  //Localhost
-  //Deploy normal project
-  base: '/some-svgs-with-inkscape-part-2/',
-  site: 'https://20essentials.github.io/some-svgs-with-inkscape-part-2/',
-  // site: 'http://localhost:4321/',
-
+  base: import.meta.env.DEV ? undefined : '/some-svgs-with-inkscape-part-2/',
+  site: import.meta.env.DEV
+    ? 'http://localhost:4321/'
+    : 'https://20essentials.github.io/some-svgs-with-inkscape-part-2/',
   integrations: [react()]
 });
