6e9d9c38d6bc268f8f11f248dd21a2129644d7ba
DominicDev <s0l0estudi4r@gmail.com>
2026-04-13 11:29:05 +0200

    v0.2.4

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,6 +1,6 @@
 import { baseUrl } from './functions';
 
-export const TOTAL_SVGS = 90;
+export const TOTAL_SVGS = 91;
 export const arraySvgs = Array.from({ length: TOTAL_SVGS }, (_, i) => ({
   url: baseUrl(`/assets/${i + 1}/${i + 1}.svg`)
 })).toSorted(() => Math.random() - 0.5);
