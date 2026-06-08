a04ab7b47b13406a3f9cae362cfa32ca8cd73fd2
DominicDev <s0l0estudi4r@gmail.com>
2026-06-08 10:01:57 +0200

    v0.3.9

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,5 +1,4 @@
 import { baseUrl } from './functions';
-
 export const TOTAL_SVGS = 107;
 export const arraySvgs = Array.from({ length: TOTAL_SVGS }, (_, i) => ({
   url: baseUrl(`/assets/${i + 1}/${i + 1}.svg`)
